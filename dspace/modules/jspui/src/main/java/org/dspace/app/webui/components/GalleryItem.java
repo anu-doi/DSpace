80ca52c7e91ddb6893b71d8ae0f09b69d3b4d715
Rahul Khanna <rahul.khanna@anu.edu.au>
2015-09-10 13:41:16 +1000

    Use getters and setters instead of Lombok annotations in GalleryItem class
    Removing Lombok dependency for consistency with the rest of the project.

--- dspace/modules/jspui/src/main/java/org/dspace/app/webui/components/GalleryItem.java
+++ dspace/modules/jspui/src/main/java/org/dspace/app/webui/components/GalleryItem.java
@@ -1,17 +1,54 @@
 package org.dspace.app.webui.components;
 
-import lombok.Getter;
-import lombok.Setter;
-
-
 /**
  * @author Osama Alkadi
  */
 public class GalleryItem {
 	
-	@Getter @Setter private String url;
-	@Getter @Setter private String title;
-	@Getter @Setter private String handle;
-	@Getter @Setter private String next;
-	@Getter @Setter private String prev;
+	private String url;
+	private String title;
+	private String handle;
+	private String next;
+	private String prev;
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getHandle() {
+		return handle;
+	}
+
+	public void setHandle(String handle) {
+		this.handle = handle;
+	}
+
+	public String getNext() {
+		return next;
+	}
+
+	public void setNext(String next) {
+		this.next = next;
+	}
+
+	public String getPrev() {
+		return prev;
+	}
+
+	public void setPrev(String prev) {
+		this.prev = prev;
+	}
+
 }
